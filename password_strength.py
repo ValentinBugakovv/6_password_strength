14b8b46e0ef330b89b94382a415f4834426044aa
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2018-03-22 20:10:42 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -5,6 +5,9 @@ black_list = ["Password1"]
 
 def get_password_strength(password):
     count = 0
+    if password in black_list:
+        count = 1
+        return count
     if re.search("\d+", password) is not None:
         count += 2
     if re.search("[A-Z]", password) is not None:
@@ -13,15 +16,13 @@ def get_password_strength(password):
         count += 2
     if re.search("[a-z]", password) is not None:
         count += 2
-    if password in black_list:
-        count = 1
     if len(password) >= 10:
         count += 2
     return count
 
 
 def main():
-    _password = str(input("input your password\n"))
+    _password = input("input your password\n")
     security_result = get_password_strength(_password)
     print("Your password strength is {}".format(security_result))
 
