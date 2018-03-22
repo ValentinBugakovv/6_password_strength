3134591eaf9acfe5069d20030d5c6f64855ada41
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2018-03-22 20:49:16 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -3,28 +3,32 @@ import re
 black_list = ["Password1"]
 
 
+def check_basic_rules_password(password):
+    count = 0
+    if password in black_list or len(password) <= 4:
+        count = 1
+    return count
+
+
 def get_password_strength(password):
     count = 0
-    if password in black_list:
-        count = 1
-        return count
     if re.search("\d+", password):
         count += 2
     if re.search("[A-Z]", password):
-        count += 2
+        count += 3
     if re.search("\W", password):
-        count += 2
+        count += 3
     if re.search("[a-z]", password):
-        count += 2
-    if len(password) >= 10:
         count += 2
     return count
 
 
 def main():
     _password = input("input your password\n")
-    security_result = get_password_strength(_password)
-    print("Your password strength is {}".format(security_result))
+    if check_basic_rules_password(_password) != 1:
+        security_result = get_password_strength(_password)
+        print("Your password strength is {}".format(security_result))
+    print("Protecting your password is 1. Change the password!")
 
 
 if __name__ == "__main__":
